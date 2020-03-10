4fd9ea4d8aada47d9a57aba38b5b91579600d5c7
Emma Bindi <emma.bindi@outlook.com>
2020-03-11 09:24:15 +1100

    Added rescue function for the budget input if user error occurs. Also added the TTY Prompt gem and implemented select prompt for the budget choices available so more user friendly with strings rather than integer input only

--- wine.rb
+++ wine.rb
@@ -1,4 +1,8 @@
 require_relative "wine_classes"
+require 'tty-prompt'
+require 'csv'
+
+prompt = TTY::Prompt.new
 
 puts "Hello and welcome to Em's Wine Selection Assistant"
 puts "Please input your age in years"
@@ -10,44 +14,78 @@ if age < 18
   exit
 else 
  puts "Thanks, let's continue"
- puts "Please confirm your budget for this wine selection:
- 1. Budget ($15 - $25)
- 2. Mid-Range ($25 - $50)
- 3. Premium (>$50)"
 
- budget = gets.chomp.to_i
 
- case budget 
 
- when 1 
-  budget == 1
-  puts "You're looking for budget"
- when 2
-  budget == 2
-  puts "You're looking for mid-Range"
- when 3
-  budget == 3
-  puts "You're fancy"
- else 
-  puts "Sorry, that input wasn't correct, please try again and enter 1, 2 or 3"
-# HOW DO I NOW GET BACK TO THE START TO RETRY BUDGET
- end
+#  Asking question with list of options couldn't be easier using select like so:
+
+# prompt.select("Choose your destiny?", %w(Scorpion Kano Jax))# =>
+# # Choose your destiny? (Use ↑/↓ arrow keys, press Enter to select)
+# # ‣ Scorpion
+# #   Kano
+# #   Jax
+
+
+budget = prompt.select("Please confirm your budget for this wine selection:", %w(Budget Mid-Range Premium))
+
+ begin
+#   puts "Please confirm your budget for this wine selection:
+#   1. Budget (<$25)
+#   2. Mid-Range ($25 - $50)
+#   3. Premium (>$50)"
+
+  # budget = gets.chomp.to_i
+
+  raise "invalid input" if budget != "Budget" && budget != "Mid-Range" && budget != "Premium"
+  rescue RuntimeError
+      puts "Sorry, that didn't work, please chose either Budget, Mid-Range or Premium from the list of options"
+      retry
+  end
 end
+#  case budget 
+
+#  when 1 
+#   budget == Budget
+#   puts "No problems. We can find something budget for you."
+#  when 2
+#   budget == Mid-Range
+#   puts "No problems, you're looking for a mid-range bottle"
+#  when 3
+#   budget == Premium
+#   puts "Ooh you're fancy"
+#  else 
+#   puts "Sorry, that input wasn't correct, please try again and enter 1, 2 or 3"
+#  end
 
 
-
+puts ""
 puts "Now that we've confirmed your price range. 
 Please confirm your preference for wine today..
 Sparkling, White, Rose, Orange or Red"
 
+puts ""
+type_selection = gets.chomp.capitalize!
+puts ""
 
-@type = gets.chomp.capitalize!
+# def which_type
+#   if @type.include? "Orange"
+#     puts "You chose Orange"
+#   else
+#   end
+# end
 
-def which_type
-  if @type.include? "Orange"
-    puts "You chose Orange"
-  else
-  end
+# which_type
+
+
+csv_text = File.read('Wine-List-March2020.csv')
+csv = CSV.parse(csv_text, :headers => true)
+# wine_selected = []
+csv.each do |row|
+  row_data = row.to_hash
+  if (row_data['Type'] == type_selection) && (row_data['Budget'] == budget)
+    puts row_data
+    # wine_selected << row_data
+  end 
 end
 
-which_type
+puts csv
--- wine_classes.rb
+++ wine_classes.rb
@@ -1,20 +1,34 @@
 class Wine 
 
-  attr_accessor :type, :name, :budget, :sale_price, :producer, :country_origin, :grape_variety
+  attr_accessor :type, :name, :budget, :sale_price, :producer, :country_of_origin, :grape_variety
 
-  type = ['Sparkling', 'White', 'Rose', 'Orange', 'Red']
+  type = ['Sparkling', 'White', 'Rose', 'Orange', 'Red', 'Surprise']
 
-  def initialize(type, name, budget, sale_price, producer, country_origin, grape_variety)
+  def initialize(type, name, budget, sale_price, producer, country_of_origin, grape_variety)
     @type = type
     @name = name
     @budget = budget 
     @sale_price = sale_price
     @producer = producer
-    @country_origin = country_origin
+    @country_of_origin = country_of_origin
     @grape_variety = grape_variety
   end
 end
 
+#Sparkling
+
+
+
+#White
+
+rfreak = Wine.new("White", "Eden Valley Riesling No 4", 1, $24, "Riesling Freak", "Australia", "Riesling")
+
+goisot = Wine.new("White", "Saint Bris Goisot", 3, "$74", "Domaine Goisot", "France", "Sauvignon Blanc")
+
+#Rose
+
+#Orange
+
 amrit = Wine.new("Orange", "Amrit", 2, "$45", "Avani", "Australia", "Skin Contact Pinot Gris")
 
-saintbris = Wine.new("White", "Saint Bris", 3, "$74", "Domaine Goisot", "France", "Sauvignon Blanc")
+#Red

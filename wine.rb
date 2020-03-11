7afa84bc4bd427610007b0362439edd24ab81316
Emma Bindi <emma.bindi@outlook.com>
2020-03-11 13:42:01 +1100

    includes a prompt to ask user if they are happy with the suggestion of if they would like to start over again. Rescue function allows this and is working. Colorize puts of suggestion into blue

--- wine.rb
+++ wine.rb
@@ -1,6 +1,8 @@
 require_relative "wine_classes"
 require 'tty-prompt'
 require 'csv'
+require 'colorize'
+require 'artii'
 
 prompt = TTY::Prompt.new
 
@@ -13,7 +15,9 @@ csv.each do |row|
 end
 
 #The below code is to collect the user's age and verify that they are over 18 years old and legally allowed to consume alcohol (within Australia)
-  
+
+begin
+
 begin 
   
   puts "Hello and welcome to Em's Wine Selection Assistant"
@@ -55,8 +59,6 @@ begin
     budget == wine.budget
   end
 
-  # p  user_wine_selection #this line prints out the new array of filtered wines
-
   type_selected = prompt.select("Now that we've confirmed your price range. Please confirm your preference for wine today:
     ", 
     %w(Sparkling White Rose Orange Red Surprise))
@@ -67,14 +69,27 @@ final_selection = user_wine_selection.select do | wine |
   type_selected == wine.type
 end
 
+#the below code is to allow for conversational style printing of the wine suggestion
+
 puts "This is the wine I will suggest today based on your budget and preferences: 
 
 #{final_selection[0].name} by #{final_selection[0].producer} which retails for #{final_selection[0].sale_price}. 
 
 This wine is from #{final_selection[0].region} in #{final_selection[0].country_of_origin} and is made from #{final_selection[0].grape_variety} grapes.
-" 
 
+".colorize(:light_blue) 
 
+# the blue code is to send the user back to the beginning of the process if they are not happy with the suggestion or changed their mind on budget
+
+satisfied = prompt.select("Would you like to accept this suggestion or review the options again and make changes to your preferences? 
+  ", ["I'm happy with my selection and would like to purchase this wine, cheers!", "I would like to review the options again"])
+
+  raise "Ok we'll start again" if satisfied != "I'm happy with my selection and would like to purchase this wine, cheers!"
+  rescue
+    retry 
+  else
+    exit
+  end
 
 #The below code is me attempting to list the wine selected into a sales file recording prices
 
